e671bfac8e481b6d7e8f56ea7658eaa5f6c2f9a9
Aleksandr Podmazko <stdfire@gmail.com>
2023-06-17 18:18:07 +0300

    initial partials + styles

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,10 @@
 import '../css/main.css'
 // import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <h1>Hello Vite!</h1>
-  </div>
-`
+// document.querySelector('#app').innerHTML = `
+//   <div>
+//     <h1>Hello Vite!</h1>
+//   </div>
+// `
 
 // setupCounter(document.querySelector('#counter'))
