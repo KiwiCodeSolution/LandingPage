767ef787c9db4a8c3a86f43bedf46233884609d3
Aleksandr Podmazko <stdfire@gmail.com>
2023-06-25 12:01:13 +0300

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import handlebars from 'vite-plugin-handlebars';
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
 
-export default defineConfig({
-  base: "/kiwicode/",
+export default {
+  base: "https://kiwicodesolution.github.io/LandingPage/",
   plugins: [handlebars({
       partialDirectory: resolve(__dirname, 'src/partials'),
     })],
-});
+};
