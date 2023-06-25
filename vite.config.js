3014058826c136b376ce6d1d3af2d5e93456f5d8
Aleksandr Podmazko <stdfire@gmail.com>
2023-06-25 15:33:08 +0300

    base url "/"

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import handlebars from 'vite-plugin-handlebars';
 import { resolve } from 'path';
 
 export default {
-  base: "/LandingPage/",
+  base: "/",
   plugins: [handlebars({
       partialDirectory: resolve(__dirname, 'src/partials'),
     })],
