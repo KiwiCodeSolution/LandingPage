a75b53f89f597bf49db12638d2b34061303581a4
Aleksandr Podmazko <stdfire@gmail.com>
2023-06-25 12:10:54 +0300

    vite config base:"LandingPage"

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import handlebars from 'vite-plugin-handlebars';
 import { resolve } from 'path';
 
 export default {
-  base: "https://kiwicodesolution.github.io/LandingPage/",
+  base: "/LandingPage/",
   plugins: [handlebars({
       partialDirectory: resolve(__dirname, 'src/partials'),
     })],
