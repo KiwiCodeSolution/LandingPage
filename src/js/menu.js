484cf34a3dc0aab128ea22ce2520c743b5f1f09a
Evgenija-P <e.a.poduzova@gmail.com>
2023-07-02 17:34:35 +0300

    modal + footer

--- src/js/menu.js
+++ src/js/menu.js
@@ -6,6 +6,8 @@ const refs = {
   headerBtns: document.querySelector(".header_buttons"),
   menu: document.querySelector(".nav--mobile"),
   body: document.querySelector("body"),
+  upBtn: document.querySelector(".go-up"),
+  downBtn: document.querySelector(".go-down"),
 };
 
 refs.openMenuBtn.addEventListener("click", toggleMenu);
@@ -15,6 +17,8 @@ function toggleMenu() {
   refs.headerBtns.classList.toggle("is-hidden");
   refs.logo.classList.toggle("is-hidden");
   refs.menu.classList.toggle("is-hidden");
+  refs.upBtn.classList.toggle("is-hidden");
+  refs.downBtn.classList.toggle("is-hidden");
   refs.body.classList.toggle("disable-scroll");
 }
 
