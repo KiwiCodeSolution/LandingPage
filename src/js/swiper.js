cc27482f0bfc9440feaf847399d4710fec072c24
Evgenija-P <99011054+Evgenija-P@users.noreply.github.com>
2023-07-05 18:05:29 +0300

    fix swipper, hero, up-button

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -3,11 +3,12 @@ const swiper = new Swiper(".swiper", {
 
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
   },
 
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".button-next",
+    prevEl: ".button-prev",
   },
 
   scrollbar: {
